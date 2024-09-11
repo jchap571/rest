4d183a4b31e04f33e5e2f31ec1d264b451ec27bb
James Chapman <jchapman605@gmail.com>
2024-09-11 15:29:57 -0600

    getting locations

--- src/controllers/LocationsController.js
+++ src/controllers/LocationsController.js
@@ -1,3 +1,4 @@
+import { locationsService } from "../services/LocationsService.js";
 import BaseController from "../utils/BaseController.js";
 
 
@@ -6,7 +7,18 @@ export class LocationsController extends BaseController {
   constructor() {
     super('api/locations')
     this.router
+      .get('', this.getLocations)
+  }
 
+
+
+  async getLocations(request, response, next) {
+    try {
+      const locations = await locationsService.getLocations()
+      response.send(locations)
+    } catch (error) {
+      next(error)
+    }
   }
 
 
--- src/services/LocationsService.js
+++ src/services/LocationsService.js
@@ -2,6 +2,10 @@ import { dbContext } from "../db/DbContext.js";
 
 
 class LocationsService {
+  async getLocations() {
+    const locations = await dbContext.Locations.find()
+    return locations
+  }
 
 
 
